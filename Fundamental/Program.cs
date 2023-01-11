47732bb47d8f886b9b96a7b9840a08fa78d85e5f
kangdiancp <kangdiancp@gmail.com>
2023-01-11 10:40:04 +0700

    day01

---
+++ Fundamental/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Fundamental // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           /* Day01.IntroConsole.ShowConsole();
+            Day01.DataTypes.ShowDataTypeRange();
+            Day01.DataTypes.ShowDiffDate();
+            Day01.ControlStatement.GuessOddEven();
+
+            Day01.Iterators.ShowBox();*/
+            Day01.Iterators.ShowOneDiagonal();
+            Day01.HandlingException.ShowTryCath();
+        }
+
+      
+    }
+}
